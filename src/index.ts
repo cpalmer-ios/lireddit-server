27820c64a1f8a0495bfa4333d8ecb865f57c1445
cwoolf123 <chriswoolf@mail.com>
2021-02-06 22:23:44 +0000

    migrated from mikroorm to  typeorm for easier conversion of types with resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
-import { MikroORM } from "@mikro-orm/core";
 import { COOKIE_NAME, __prod__ } from "./constants"; 
-import microConfig from "./mikro-orm.config";
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
@@ -13,10 +11,23 @@ import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import cors from 'cors';
+import { createConnection } from 'typeorm';
+import { Post } from './entities/Post';
+import { User } from './entities/User';
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
+   await createConnection({
+        type: 'postgres',
+        database: 'lireddit2',
+        username: 'webdev',
+        password: 'Ezekias000',
+        logging: true,
+        synchronize: true,
+        entities: [Post, User]
+    });
+
+    // const orm = await MikroORM.init(microConfig);
+    // await orm.getMigrator().up();
 
     const app = express();
 
@@ -54,7 +65,7 @@ const main = async () => {
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false,
         }),
-        context: ({req, res}) => ({ em: orm.em, req, res, redis })
+        context: ({req, res}) => ({ req, res, redis })
     });
 
     apolloServer.applyMiddleware({ 
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,12 +2,12 @@ import { Resolver, Mutation, Arg, Field, Ctx, ObjectType, Query } from 'type-gra
 import { MyContext } from '../types';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
-import { EntityManager } from '@mikro-orm/postgresql'
 import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants';
 import { UsernamePasswordInput } from './UsernamePasswordInput';
 import { validateRegister } from '../utils/validateRegister';
 import { sendEmail } from '../utils/sendEmail';
 import { v4 } from 'uuid';
+import { getConnection } from 'typeorm';
 @ObjectType()
 class FieldError {
     @Field()
@@ -32,7 +32,7 @@ export class UserResolver {
     async changePassword(
         @Arg("token") token: string,
         @Arg("newPassword") newPassword: string,
-        @Ctx() { redis, em, req }: MyContext
+        @Ctx() { redis, req }: MyContext
     ): Promise<UserResponse> {
         if (newPassword.length <= 2) {
             return {
@@ -58,7 +58,8 @@ export class UserResolver {
             };
         }
 
-        const user = await em.findOne(User, { id: parseInt(userId) });
+        const userIdNum = parseInt(userId)
+        const user = await User.findOne(userIdNum);
 
         if (!user) {
             return {
@@ -71,8 +72,12 @@ export class UserResolver {
             };
         }
 
-        user.password = await argon2.hash(newPassword);
-        await em.persistAndFlush(user);
+        await User.update(
+            { id: userIdNum },
+            {
+                password: await argon2.hash(newPassword),
+            }
+        );
 
         await redis.del(key);
 
@@ -83,8 +88,10 @@ export class UserResolver {
     }
 
     @Mutation(() => Boolean)
-    async forgotPassword(@Arg('email') email: string, @Ctx() { em, redis }: MyContext) {
-        const user = await em.findOne(User, { email });
+    async forgotPassword(@Arg('email') email: string,
+        @Ctx() { redis }: MyContext
+    ) {
+        const user = await User.findOne({ where: { email } });
         if (!user) {
             //no email in the db
             return true;
@@ -109,18 +116,17 @@ export class UserResolver {
 
 
     @Query(() => User, { nullable: true })
-    async me(@Ctx() { req, em }: MyContext) {
+    me(@Ctx() { req }: MyContext) {
         if (!req.session.userId) {
             return null
         }
-        const user = await em.findOne(User, { id: req.session.userId });
-        return user;
+        return User.findOne(req.session.userId);
     }
 
     @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { req }: MyContext
     ): Promise<UserResponse> {
         const errors = validateRegister(options);
         if (errors) {
@@ -128,29 +134,23 @@ export class UserResolver {
         }
         // add regex here
         const hashedPassword = await argon2.hash(options.password)
-        // const user = em.create(User, { 
-        //     username: options.username, 
-        //     password: hashedPassword,
-        //  });
         let user;
-
         try {
-            const result = await (em as EntityManager)
-                .createQueryBuilder(User)
-                .getKnexQuery()
-                .insert(
-                    {
-                        username: options.username,
-                        email: options.email,
-                        password: hashedPassword,
-                        created_at: new Date(),
-                        updated_at: new Date()
-
+            const result = await getConnection()
+                .createQueryBuilder()
+                .insert()
+                .into(User)
+                .values({
+                    username: options.username,
+                    email: options.email,
+                    password: hashedPassword,
                     })
-                .returning('*');
-            // await em.persistAndFlush(user);
-            user = result[0];
+                .returning("*")
+                .execute();
+            // console.log("result:", result);
+            user = result.raw[0];
         } catch (err) {
+            // console.log(err)
             if (err.code === '23505' || err.detail.includes("already exists")) {
                 //duplicate username error
                 return {
@@ -162,6 +162,8 @@ export class UserResolver {
             }
             console.log("message: ", err.message);
         }
+        req.session.userId = user.id;
+
         return { user };
     }
 
@@ -169,10 +171,14 @@ export class UserResolver {
     async login(
         @Arg("usernameOrEmail") usernameOrEmail: string,
         @Arg("password") password: string,
-        @Ctx() { em, req }: MyContext
+        @Ctx() { req }: MyContext
     ) {
         //Regex email here
-        const user = await em.findOne(User, usernameOrEmail.includes('@') ? { email: usernameOrEmail } : { username: usernameOrEmail });
+        const user = await User.findOne(
+            usernameOrEmail.includes('@')
+                ? { where: { email: usernameOrEmail } }
+                : { where: { username: usernameOrEmail } }
+        );
         if (!user) {
             return {
                 errors: [{
